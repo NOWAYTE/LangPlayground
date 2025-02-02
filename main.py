863e7675fde7392fb965f705156a3e193fe85fd6
Daniel kamau <danielkamaunganga3@gmail.com>
2025-02-02 22:58:14 +0300

    llm with tools

--- main.py
+++ main.py
@@ -1,23 +1,70 @@
-#!/usr/bin/env python3
-
 from typing import Annotated
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+import os
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import BaseMessage
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
-from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import ToolNode, tools_condition
+
+load_dotenv()
 
 
-class State(TypeDict):
+class State(TypedDict):
     messages: Annotated[list, add_messages]
+
 
 graph_builder = StateGraph(State)
 
 
-llm = ChatOpenAI(model="gpt-4o")
+tool = TavilySearchResults(
+    api_key=os.getenv("TAVILY_API_KEY"),
+    max_results=2)
+tools = [tool]
+llm = ChatOpenAI(
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
+    model="gpt-4o")
+llm_with_tools = llm.bind_tools(tools)
+
 
 def chatbot(state: State):
-    return { "messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
 
 graph_builder.add_node("chatbot", chatbot)
 
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
 graph = graph_builder.compile()
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
